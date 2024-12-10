5ce9637115c8e0eb5925e5f4594c6ec94de3606e
emanuelpereira97 <efelipemp15@hotmail.com>
2024-12-10 16:41:18 -0300

    questão 1 da priemira lista avaliativa

---
+++ AvaliativaComandoCondicional/q1.py
@@ -0,0 +1,17 @@
+altura = float(input("Digite sua altura em metros: "))
+peso = float(input("Digite seu peso em quilograma: "))
+
+imc = round(peso/(altura**2),2)
+
+if imc <= 18.5:
+    print("Seu IMC é de: ",imc,". Esse valor é cosiderado abaixo do normal.")
+elif imc >= 18.6 and imc <= 24.9:
+    print("Seu IMC é de: ",imc,". Esse valor é cosiderado normal.")
+elif imc >= 25 and imc <= 29.9:
+    print("Seu IMC é de: ",imc,". Esse valor é cosiderado sobrepeso.")
+elif imc >= 30 and imc <= 34.9:
+    print("Seu IMC é de: ",imc,". Esse valor é cosiderado obsidade grau 1.")
+elif imc >= 35 and imc <= 39.9:
+    print("Seu IMC é de: ",imc,". Esse valor é cosiderado obsidade grau 2.")
+elif imc >= 40:
+    print("Seu IMC é de: ",imc,". Esse valor é cosiderado obsidade grau 3.")
