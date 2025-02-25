2e0d44afab569c3df04ddb100ca42d11297ff972
emanuelpereira97 <efelipemp15@hotmail.com>
2025-02-25 13:02:49 -0300

    exercicios

--- Listas/naoAvaliativo/questao.py
+++ Listas/naoAvaliativo/questao.py
@@ -5,14 +5,14 @@ retornado como troco.'''
 valor_conta = int(input("Digite. Quanto custou a sua conta? "))
 pagamento = int(input("Digite. Com quanto você vai pagar? "))
 
-notas = [200,100,50,20,10,5,2,1]
+notas = {200:0,100:0,50:0,20:0,10:0,5:0,2:0,1:0}
 troco = pagamento - valor_conta
-notas_usadas = []
+
 print(f'Seu troco é R${troco}')
-for nota in notas:
+for nota in notas.keys():
     qtd_cedula = troco // nota
     troco =(troco - (qtd_cedula*nota))
     if qtd_cedula != 0:
-        notas_usadas.append(nota)
-    print(f'Quatidade notas de ${nota} : {qtd_cedula}')
-print(f'Sugestão de troco:{notas_usadas}')
+        notas[nota] = qtd_cedula
+    #print(f'Quatidade notas de ${nota} : {qtd_cedula}')
+print(f'Sugestão de troco:{notas}')
--- Listas/naoAvaliativo/questao2.py
+++ Listas/naoAvaliativo/questao2.py
@@ -6,6 +6,12 @@ b= 4 #int(input("Digite um valor inteiro para o coeficiente b: "))
 c= 1 #int(input("Digite um valor inteiro para o coeficiente c: "))
 
 delta = (b*b) - (4*a*c) 
-if (delta > 0 or delta == 0):
-
-    print(delta)
+if (delta == 0):
+    x1 = (-b)/(2*a)
+    print(f'Como delta é ={delta}, por isso X1 e X2 são iguais à: {x1}')
+elif (delta > 0):
+    x1 = (-b+(delta**(1/2)))/(2*a)
+    x2 = (-b-(delta**(1/2)))/(2*a)
+    print(f'Como delta positivo e igual à {delta}, X1 é {x1}  e X2 é {x2}')
+elif (delta < 0):
+    print(f'Como delta é negativo, não existem raizes reais.')
