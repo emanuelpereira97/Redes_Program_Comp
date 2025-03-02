a7f2e9505988b0bc06a734a178802802b6878c89
emanuelpereira97 <efelipemp15@hotmail.com>
2025-03-02 15:12:56 -0300

    JogOvo

--- joguin/test1.py
+++ joguin/test1.py
@@ -4,18 +4,25 @@ from sys import exit #importando a função exit da biblioteca sys
 
 pygame.init() #inicializa a biblioteca pygame
 
-largura = 640 #largura da tela
-altura = 480 #altura da tela
-
-tela = pygame.display.set_mode((largura,altura)) #definições do objeto Tela
 pygame.display.set_caption('O jogovo')
 
+largura = 640 #largura da tela
+altura = 480 #altura da tela
+tela = pygame.display.set_mode((largura,altura)) #definições do objeto Tela
+
+branco = (255,255,255)
+nuvem_esq = [pygame.draw.ellipse(tela,branco, (10,200,150,45)), pygame.draw.ellipse(tela,branco, (3,175,60,58)), pygame.draw.ellipse(tela,branco, (50,185,60,40))]
+nuvem_dir = [pygame.draw.ellipse(tela,branco, (476,200,150,45)), pygame.draw.ellipse(tela,branco, (576,175,60,58)), pygame.draw.ellipse(tela,branco, (520,185,60,40))]
+ovo = pygame.draw.ellipse(tela,branco, (300,200,30,45)) #desenha um circulo, paramentros semelhantes, troca L*H por raio
+
+x = 0
+y = 0
+
 while True: #loop principal, o jogo roda aqui
-    for event in pygame.event.get(): #esse loop verifica se algum evento ocorreu (clicou, teclou ...)
+    for event in pygame.event.get(): #loop principal verifica se algum evento ocorreu (clicou, teclou ...)
         if event.type == QUIT:
             pygame.quit()
             exit()
-    pygame.draw.rect(tela,(255,0,125), (320,240,30,50)) #desenha retangulo p(onde,cor,(posição xy, Largura e altura))
-    pygame.draw.circle(tela,(255,255,255), (160,120), 35) #desenha um circulo, paramentros semelhantes, troca L*H por raio
-    pygame.draw.line(tela,(100,120,140),(500,0),(0,400),5) #desenha linha, ponto inicial, ponto final e espessura
+    
+    
     pygame.display.update() #atualiza a tela a cada interação
