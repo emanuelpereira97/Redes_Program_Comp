0f7768f93c8b93bca575f293b4785a0f4a8a72d6
emanuelpereira97 <efelipemp15@hotmail.com>
2025-03-02 15:48:48 -0300

    JogOvo

--- joguin/test1.py
+++ joguin/test1.py
@@ -4,25 +4,30 @@ from sys import exit #importando a função exit da biblioteca sys
 
 pygame.init() #inicializa a biblioteca pygame
 
-pygame.display.set_caption('O jogovo')
+pygame.display.set_caption('JogOvo')
 
 largura = 640 #largura da tela
 altura = 480 #altura da tela
 tela = pygame.display.set_mode((largura,altura)) #definições do objeto Tela
 
 branco = (255,255,255)
-nuvem_esq = [pygame.draw.ellipse(tela,branco, (10,200,150,45)), pygame.draw.ellipse(tela,branco, (3,175,60,58)), pygame.draw.ellipse(tela,branco, (50,185,60,40))]
-nuvem_dir = [pygame.draw.ellipse(tela,branco, (476,200,150,45)), pygame.draw.ellipse(tela,branco, (576,175,60,58)), pygame.draw.ellipse(tela,branco, (520,185,60,40))]
-ovo = pygame.draw.ellipse(tela,branco, (300,200,30,45)) #desenha um circulo, paramentros semelhantes, troca L*H por raio
+preto = (0,0,0)
+
 
 x = 0
 y = 0
 
 while True: #loop principal, o jogo roda aqui
+    tela.fill((preto))
     for event in pygame.event.get(): #loop principal verifica se algum evento ocorreu (clicou, teclou ...)
         if event.type == QUIT:
             pygame.quit()
             exit()
-    
+    nuvem_esq = [pygame.draw.ellipse(tela,branco, ((14-x),(435-y),150,45)), pygame.draw.ellipse(tela,branco, ((4-x),(409-y),60,58)), pygame.draw.ellipse(tela,branco, ((50-x),(420-y),60,40))]
+    nuvem_dir = [pygame.draw.ellipse(tela,branco, ((476-x),(435-y),150,45)), pygame.draw.ellipse(tela,branco, ((576-x),(409-y),60,58)), pygame.draw.ellipse(tela,branco, ((520-x),(420-y),60,40))]
+    ovo = pygame.draw.ellipse(tela,branco, (300,200,30,45)) #desenha um circulo, paramentros semelhantes, troca L*H por raio
+    if y >= altura:
+        y = 0
+    y += 1
     
     pygame.display.update() #atualiza a tela a cada interação
